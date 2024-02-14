1ae459bac3b932deb0f015a4aac55aee0ff82196
zaraa28 <zara09.starc@gmail.com>
2024-02-14 15:01:20 +0100

    pocetak zanimljivosti

--- app/src/main/java/com/example/nekikviz/zanimljivost.kt
+++ app/src/main/java/com/example/nekikviz/zanimljivost.kt
@@ -1,24 +1,25 @@
 package com.example.nekikviz
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.defaultMinSize
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
@@ -44,32 +45,42 @@ fun Zanimljivost() {
         ) {
             Text(
                 modifier = Modifier
-                    .padding(top = 20.dp),
+                    .padding(top = 35.dp),
                 text = "Jeste li znali?",
                 color = Color.White,
                 fontSize = 20.sp,
                 fontWeight = FontWeight.Bold
             )
         }
-        BasicTextField(
-            value = "Procesor (engl. central processing unit − CPU), " +
-                    "glavni je dio računala koji vođen zadanim programskim" +
-                    "naredbama izvodi osnovne radnje nad podacima.",
-            onValueChange = {},
-            enabled = false,
-            textStyle = TextStyle(
+
+        Image(
+            painter = painterResource(id = R.drawable.usklicnik),
+            contentDescription = "Uskličnik",
+            modifier = Modifier
+                .size(90.dp)
+                .padding(top = 60.dp)
+        )
+
+        Card(
+            modifier = Modifier
+                .padding(top = 300.dp),
+            colors = CardDefaults.cardColors(Color.White),
+
+            ) {
+
+            Text(
+                text = "Procesor (engl. central processing unit − CPU) " +
+                        "glavni je dio računala koji vođen zadanim programskim" +
+                        " naredbama izvodi osnovne radnje nad podacima.",
                 color = Color(0xff280a82),
                 fontSize = 28.sp,
-                fontWeight = FontWeight.Bold,
-                textAlign = TextAlign.Center
-            ),
-            modifier = Modifier
-                .align(Alignment.Center)
-                .fillMaxWidth()
-                .defaultMinSize(minHeight = 200.dp)
-                .background(Color.White, RoundedCornerShape(40.dp))
-                .padding(start = 20.dp, end = 16.dp, top = 100.dp, bottom = 100.dp)
-        )
+                fontFamily = FontFamily.Monospace,
+                fontWeight = FontWeight.Bold
+            )
+        }
+
     }
+
 }
+
 
